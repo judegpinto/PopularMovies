57fd6e027e71a3b067c4ec1db9349a55566acec0
Jude Pinto <jude.pinto@dexcom.com>
2017-08-18 22:58:33 -0700

    added sort functionality added menu spinner attached item selected listener created two separate http queries for a changed sort option

--- app/src/main/java/com/example/jp0517/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/jp0517/popularmovies/MainActivity.java
@@ -1,9 +1,8 @@
 package com.example.jp0517.popularmovies;
 
-import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
@@ -12,17 +11,23 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.jp0517.popularmovies.utilities.JsonTools;
 import com.example.jp0517.popularmovies.utilities.NetworkUtils;
 import com.example.jp0517.popularmovies.view.PosterAdapter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private static final int width = 3;
     private PosterAdapter posterAdapter;
     private RecyclerView posterView;
-    private String exampleUrl;
+    private String popularUrl = "https://api.themoviedb.org/3/movie/popular?api_key=0af7c209fbb3336d2aee22d8442bbb4f";
+    private String topRatedUrl = "https://api.themoviedb.org/3/movie/top_rated?api_key=0af7c209fbb3336d2aee22d8442bbb4f";
+    Spinner sortOption;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,17 +42,21 @@ public class MainActivity extends AppCompatActivity {
         String query = getString(R.string.dicover_popular);
         String url = getString(R.string.api_base_url) + getString(R.string.movie_key) + query;
         //should look like: https://api.themoviedb.org/3/movie/popular?api_key=0af7c209fbb3336d2aee22d8442bbb4f
-        exampleUrl = "https://api.themoviedb.org/3/movie/popular?api_key=0af7c209fbb3336d2aee22d8442bbb4f";
+
         Uri uri = Uri.parse(url);
         String uriString = uri.toString();
         Log.d(getClass().getSimpleName(),uriString);
         posterAdapter = new PosterAdapter(getApplicationContext());
         posterView.setAdapter(posterAdapter);
-        loadMovies(exampleUrl);
+        loadMoviesPopular();
     }
 
-    public void loadMovies(String url) {
-        new MovieTask().execute(url);
+    public void loadMoviesPopular() {
+        new MovieTask().execute(popularUrl);
+    }
+
+    public void loadMoviesTopRated() {
+        new MovieTask().execute(topRatedUrl);
     }
 
     //TODO: Call this task so image urls are returned in string array, then process in onpostexcute
@@ -59,9 +68,9 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-            MovieInfo[] movies = JsonTools.getMovieInfo(s);
+        protected void onPostExecute(String unparsed) {
+            super.onPostExecute(unparsed);
+            MovieInfo[] movies = JsonTools.getMovieInfo(unparsed);
             posterAdapter.setMovieInfo(movies);
         }
     }
@@ -70,16 +79,29 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
 
-        inflater.inflate(R.menu.movies,menu);
+        inflater.inflate(R.menu.sort,menu);
+        MenuItem item = menu.findItem(R.id.menuSort);
+        sortOption = (Spinner) MenuItemCompat.getActionView(item);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.sort_array,R.layout.support_simple_spinner_dropdown_item);
+        sortOption.setAdapter(adapter);
+        sortOption.setOnItemSelectedListener(this);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        if(item.getItemId()==R.id.action_refresh) {
-            loadMovies(exampleUrl);
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        switch (position) {
+            case 0:
+                loadMoviesPopular();
+                break;
+            case 1:
+                loadMoviesTopRated();
+                break;
         }
-        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
